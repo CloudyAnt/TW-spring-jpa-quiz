f634c6006e151b2c859706a5aec836ebc9b2caeb
itscloudy <itscloudy@foxmail.com>
2019-06-01 21:25:51 +0800

    set up database

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -2,4 +2,40 @@ package com.example.employee.entity;
 
 public class Employee {
 
+    int id, age, companyId, salary;
+
+    String name, gender;
+
+    public Employee(int id, String name, int age, String gender, int companyId, int salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
